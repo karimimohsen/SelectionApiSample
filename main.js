45662489e9fbe32af0018ede5d2dd7a0f6794e18
Mohsen Karimi <karimimohsen@live.com>
2018-06-15 16:20:44 +0430

    anchor

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-var m = '';
+/** */
+const container = document.querySelector('#container');
+// const selection = container.getSelection();
+
+const getSelection = () => window.getSelection();
+
+const anchorbtn = document.querySelector('#anchor');
+anchorbtn.addEventListener('click', (e) => {
+    const selection = getSelection();
+    if (selection.type !== 'Range') return;
+    
+});
