432874370a067e0e056cf523f0e1f2136832458e
Mohsen Karimi <karimimohsen@live.com>
2018-06-16 20:03:47 +0430

    anchor & focus compare

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 /** */
-const container = document.querySelector('#container');
-// const selection = container.getSelection();
-
 const getSelection = () => window.getSelection();
 
 const anchorbtn = document.querySelector('#anchor');
 anchorbtn.addEventListener('click', (e) => {
     const selection = getSelection();
     if (selection.type !== 'Range') return;
-    
+    console.log('anchor node:\r')
+    console.dir(selection.anchorNode);    
+    console.dir(selection.anchorOffset);
+
+    console.log('focus node:\r')
+    console.dir(selection.focusNode);    
+    console.dir(selection.focusOffset);
+
+    console.log(`compaire node: ${selection.anchorNode === selection.focusNode}`);
 });
