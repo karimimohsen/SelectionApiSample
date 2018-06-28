94ee6c15e81fed86d4719a9804fb903e80e0a0f0
Mohsen Karimi <karimimohsen@live.com>
2018-06-28 21:25:55 +0430

    added all range and selection api

--- main.js
+++ main.js
@@ -172,42 +172,162 @@ const selectnoderange = document.querySelector('#selectnoderange');
 selectnoderange.addEventListener('click', (e) => {
     setTimeout(() => {
         const range = document.createRange();
-        const span = document.querySelector('#container p span');    
+        const span = document.querySelector('#container p span');
         range.selectNode(span);
-        
+
         // add to selection
         const selection = window.getSelection();
         selection.removeAllRanges();
         selection.addRange(range);
-    
+
         console.group('selectNode: window.getSelection');
-        console.log(selection);    
+        console.log(selection);
         console.groupEnd();
-    
+
         const ranged = selection.getRangeAt(0);
         console.group('selectNode: window.getSelection().getRangedAt');
-        console.log(ranged);    
-        console.groupEnd();        
+        console.log(ranged);
+        console.groupEnd();
     }, 0);
 
     setTimeout(() => {
         const range = document.createRange();
-        const span = document.querySelector('#container p span');    
+        const span = document.querySelector('#container p span');
         range.selectNodeContents(span);
-        
+
         // add to selection
         const selection = window.getSelection();
         selection.removeAllRanges();
         selection.addRange(range);
-    
+
         console.group('selectNodeContents: window.getSelection');
-        console.log(selection);    
+        console.log(selection);
         console.groupEnd();
-    
+
         const ranged = selection.getRangeAt(0);
         console.group('selectNodeContents: window.getSelection().getRangedAt');
-        console.log(ranged);    
-        console.groupEnd();        
-    }, 2000);    
+        console.log(ranged);
+        console.groupEnd();
+    }, 2000);
+});
+
+const collapserange = document.querySelector('#collapserange');
+collapserange.addEventListener('click', (e) => {
+    const range = document.createRange();
+
+    const p = document.querySelector('#container p span');
+    range.selectNode(p)
+    range.collapse(true);
+
+    const selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+});
+
+const insertrange = document.querySelector('#insertrange');
+insertrange.addEventListener('click', (e) => {
+    const range = document.createRange();
+
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Mohsen';
+
+    const p = document.querySelector('#container p span');
+    range.selectNode(p);
+    range.collapse(true);
+    range.insertNode(h1);
+    // const selection = window.getSelection();
+    // selection.removeAllRanges();
+    // selection.addRange(range);
+});
+
+const comparerange = document.querySelector('#comparerange');
+comparerange.addEventListener('click', (e) => {
+    const spans = document.querySelectorAll('#container p  *');
+
+    const range = document.createRange();
+    range.selectNode(spans[0]);
+
+    const sourcerange = document.createRange();
+    sourcerange.selectNode(spans[1]);
+    console.group('-1:before  0:equal  1:after');
+    console.group('Range.START_TO_START');
+    console.log(range.compareBoundaryPoints(Range.START_TO_START, sourcerange));
+    console.groupEnd();
+
+    console.group('Range.START_TO_END');
+    console.log(range.compareBoundaryPoints(Range.START_TO_END, sourcerange));
+    console.groupEnd();
+
+    console.group('Range.END_TO_START');
+    console.log(range.compareBoundaryPoints(Range.END_TO_START, sourcerange));
+    console.groupEnd();
+
+    console.group('Range.END_TO_END');
+    console.log(range.compareBoundaryPoints(Range.END_TO_END, sourcerange));
+    console.groupEnd();
+    console.groupEnd();
+});
+
+const comparepointrange = document.querySelector('#comparepointrange');
+comparepointrange.addEventListener('click', (e) => {
+    const spans = document.querySelectorAll('#container p  *');
+
+    const range = document.createRange();
+    range.selectNode(spans[0]);
+
+    console.group('-1:before  0:equal  1:after');
+    console.log(range.comparePoint(spans[0], 0));
+    console.groupEnd();
+});
+
+const contextualfragmentrange = document.querySelector('#contextualfragmentrange');
+contextualfragmentrange.addEventListener('click', (e) => {
+    const tagString = `<h2>Hello</h2>`;
+    const range = document.createRange();
+
+    const doc = range.createContextualFragment(tagString);
+    document.body.appendChild(doc);
+});
+
+const clonerange = document.querySelector('#clonerange');
+clonerange.addEventListener('click', (e) => {
+    const node = document.querySelector('#container p span');
+    const range = document.createRange();
+    range.selectNode(node);
+    const clone = range.cloneRange();
+    clone.setEnd(node, 2);
+
+    const selection = getSelection();
+    selection.removeAllRanges();
+    selection.addRange(clone);
+});
+
+const surroundContents = document.querySelector('#surroundContents');
+surroundContents.addEventListener('click', (e) => {
+    const node = document.querySelector('#container p span');
+    const range = document.createRange();
+    range.selectNode(node);
+
+    const bold = document.createElement('b');
+    range.surroundContents(bold);
+});
+
+const deleteContentsrange = document.querySelector('#deleteContentsrange');
+deleteContentsrange.addEventListener('click', (e) => {
+    const node = document.querySelector('#container p span');
+    const range = document.createRange();
+    
+    range.selectNode(node);
+    range.deleteContents();
+});
+
+const extractContentsrange = document.querySelector('#extractContentsrange');
+extractContentsrange.addEventListener('click', (e) => {
+    const node = document.querySelector('#container p span');
+    const range = document.createRange();
+    
+    range.selectNode(node);
+    const move = range.extractContents();
+    document.querySelector('#container').appendChild(move);
 });
 
